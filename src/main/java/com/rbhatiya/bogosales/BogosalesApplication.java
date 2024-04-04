a38499a6510011ba35190c911425c6e00de36426
Rajesh-Code <raju.bhatiya@gmail.com>
2024-04-04 10:36:14 +0530

    first commit

---
+++ src/main/java/com/rbhatiya/bogosales/BogosalesApplication.java
@@ -0,0 +1,13 @@
+package com.rbhatiya.bogosales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BogosalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BogosalesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rbhatiya/bogosales/BogosalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rbhatiya.bogosales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BogosalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
